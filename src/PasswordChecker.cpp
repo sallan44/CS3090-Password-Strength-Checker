437d86be12b9b817c9ca8b50a87098353d178e2d
samuelallan <sallan1844@gmail.com>
2026-02-11 14:55:47 -0700

    Wrote password checker program, need to make makefile

---
+++ src/PasswordChecker.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <regex>
+
+class PasswordChecker{
+    private:
+        //name of user
+        std::string name;
+        //password inputted
+        std::string password;
+        //message to be displayed to user upon entering password
+        std::string message;
+        //regex expression for special characters
+        std::regex specialCharacters{"[^a-zA-Z0-9]"};
+        //regex expression for a space
+        std::regex spaces{"\\s"};
+
+    public:
+
+        PasswordChecker(std::string name, std::string password) : name(name), password(password) {};
+
+        void checkPasswordRequirements(){
+            if (password.length() < 10){
+                 message = "Password must be at least 10 characters.";
+            } else if (std::regex_search(password, spaces)){
+                message = "Password can not contain spaces.";
+            } else if (!std::regex_search(password, specialCharacters)){
+                 message = "Password must have at least one special character.";
+            } else if (password.find(name) == std::string::npos){
+                message = "Password must not contain name.";
+            } else {
+                message = "Password is strong.";
+            }
+        }
+
+        void displayMessage(){
+            std::cout << message << std::endl;
+        }
+};
+
+int main(){
+    std::string name; 
+    std::string password; 
+    std::cout << "Enter your name: " << std::endl;
+    std::cin >> name;
+    std::cout << "Enter a password: " << std::endl;
+    std::cin >> password;
+
+    PasswordChecker pc(name, password);
+    pc.checkPasswordRequirements();
+    pc.displayMessage();
+
+    return 0;
+}
